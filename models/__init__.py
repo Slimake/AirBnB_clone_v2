77f481af1d07297488a050b0c80f3c9b8fb6a7df
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-07-27 15:55:17 +0100

    Update file to contain DBStorage and FileStorage

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,28 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+from models.base_model import BaseModel
+from os import getenv
 
 
-storage = FileStorage()
+envv = getenv('HBNB_TYPE_STORAGE')
+if envv == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+
+classes = {
+    'BaseModel': BaseModel, 'User': User,
+    'Place': Place, 'State': State,
+    'City': City, 'Amenity': Amenity,
+    'Review': Review
+}
+
 storage.reload()
