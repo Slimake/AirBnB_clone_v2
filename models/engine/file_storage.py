447eb8f254b752b0ad4247bff5d85c1589820736
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-07-27 15:49:25 +0100

    Modify all and reload method in file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
-
+import models
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
@@ -10,8 +10,8 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        objs = {}  # store objs of one type of class
         if cls:  # cls is not empty
-            objs = {}  # store objs of one type of class
             for key, value in FileStorage.__objects.items():
                 if cls.__name__ == value.to_dict()['__class__']:
                     objs.update({key: value.to_dict()})
@@ -33,25 +33,12 @@ class FileStorage:
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
-        classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                    self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = models.classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
 
