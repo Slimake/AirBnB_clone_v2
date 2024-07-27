23e40bbee1c68f89f10385fa52f29017c2e089d1
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-07-27 16:12:10 +0100

    Fix pycodestyle error

--- models/state.py
+++ models/state.py
@@ -14,7 +14,7 @@ class State(BaseModel, Base):
 
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='states', 
+        cities = relationship('City', backref='states',
                               cascade='all, delete, delete-orphan')
     else:
         name = ''
