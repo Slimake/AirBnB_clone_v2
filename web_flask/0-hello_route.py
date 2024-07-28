9a495e6c97664240205d9eabbdf036011f424e85
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-07-28 16:33:11 +0100

    Add port number and documentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,8 +8,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    """Route for Homepage"""
     return "Hello HBNB"
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port=5000)
