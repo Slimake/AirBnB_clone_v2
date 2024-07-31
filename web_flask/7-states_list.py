0da038b0edef2b4402648d1275845abbd12308e8
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-07-31 23:57:56 +0100

    Fetch data, remove current SQLAlchemy Session, create /states_list route

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+7-states_list Module
+"""
+from flask import Flask, render_template
+from models import storage, classes
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Closes the database again at the end of the request"""
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """Display a HTML page"""
+    states = storage.all(classes['State']).items()
+    states = dict(sorted(states))
+
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
