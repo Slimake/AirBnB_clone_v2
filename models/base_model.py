14bd99cffbe6cef849753a651ebdbff08ad588bf
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-07-31 18:48:06 +0100

    Check if key exist kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -23,11 +23,24 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+            if kwargs.get('updated_at'):
+                kwargs['updated_at'] = datetime \
+                        .strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            else:
+                kwargs['updated_at'] = datetime.now()
+
+            if kwargs.get('created_at'):
+                kwargs['created_at'] = datetime \
+                        .strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            else:
+                kwargs['created_at'] = datetime.now()
+
+            if kwargs.get('id') is None:
+                kwargs['id'] = str(uuid.uuid4())
+
+            if kwargs.get('__class__'):
+                del kwargs['__class__']
+
             self.__dict__.update(kwargs)
 
     def __str__(self):
